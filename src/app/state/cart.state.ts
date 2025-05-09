d5f0b4646c30003f419cdaf44a2d15ae4da9f44d
urosmladenovic <uros.mladenovic@levi9.com>
2025-05-09 11:26:00 +0200

    Update state and service

--- src/app/state/cart.state.ts
+++ src/app/state/cart.state.ts
@@ -1,5 +1,14 @@
-import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import {
+  patchState,
+  signalStore,
+  withHooks,
+  withMethods,
+  withProps,
+  withState,
+} from '@ngrx/signals';
 import { Product } from '../model/products.model';
+import { inject } from '@angular/core';
+import { CartService } from '../service/cart.service';
 
 type CartState = {
   cartItems: Product[];
@@ -12,21 +21,47 @@ const initialState: CartState = {
 export const CartStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
+  withProps(() => ({
+    cartService: inject(CartService),
+  })),
   withMethods((store) => {
-    const addToCart = (product: Product) => {
+    const getCartItems = () => {
+      const cartItems = store.cartService.getCart();
+
       patchState(store, {
-        cartItems: store.cartItems().some((item) => item.id === product.id)
-          ? store.cartItems()
-          : [...store.cartItems(), product],
+        cartItems,
+      });
+    };
+
+    const addToCart = (product: Product) => {
+      const cartItems = store.cartItems().some((item) => item.id === product.id)
+        ? store.cartItems()
+        : [...store.cartItems(), product];
+
+      store.cartService.setCart(cartItems);
+
+      patchState(store, {
+        cartItems,
       });
     };
 
     const removeFromCart = (productId: number) => {
+      const cartItems = store
+        .cartItems()
+        .filter((item) => item.id !== productId);
+
+      store.cartService.setCart(cartItems);
+
       patchState(store, {
-        cartItems: store.cartItems().filter((item) => item.id !== productId),
+        cartItems,
       });
     };
 
-    return { addToCart, removeFromCart };
+    return { getCartItems, addToCart, removeFromCart };
+  }),
+  withHooks({
+    onInit: (store) => {
+      store.getCartItems();
+    },
   }),
 );
