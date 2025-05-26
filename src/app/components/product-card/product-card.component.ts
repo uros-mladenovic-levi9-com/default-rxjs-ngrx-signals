4cdf0687cf19f156e08c60ee1baffde804475df8
urosmladenovic <uros.mladenovic@levi9.com>
2025-05-26 19:30:43 +0200

    Add product card component

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../model/products.model';
 
 @Component({
   selector: 'app-product-card',
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
+  @Input() product?: Product;
+  @Output() productCardClicked = new EventEmitter<number>();
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
 import { AsyncPipe } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products',
-  imports: [AsyncPipe, RouterModule],
+  imports: [AsyncPipe, RouterModule, ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
   standalone: true,
@@ -13,5 +14,10 @@ import { RouterModule } from '@angular/router';
 })
 export class ProductsComponent {
   readonly productsService = inject(ProductsService);
+  readonly router = inject(Router);
   readonly products$ = this.productsService.getAllProducts();
+
+  navigateToProductDetails(productId: number): void {
+    this.router.navigate(['/products', productId]);
+  }
 }
